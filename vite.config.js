fcf2a416fd1b0db25da61e0a4f16e0d1f16493a6
OmarMousteau <omousteau.data@gmail.com>
2025-03-17 22:51:59 +0100

    add base to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  base: '/veille_techno_blockchain_react_app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
