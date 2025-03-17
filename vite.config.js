c105dd3b5702485ca987935d4284af1db9ef2c5d
OmarMousteau <omousteau.data@gmail.com>
2025-03-17 22:44:16 +0100

    test for deploying vite react with gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  base : "/vite-deploy/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
